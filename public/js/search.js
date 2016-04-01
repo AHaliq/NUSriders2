cd8dcec9a2931e2b0d4f45de0893257cc9fbcb20
AHaliq <liqo12@hotmail.com>
2016-04-02 01:58:42 +0800

    updated todo list to achieve completion of all things route/advertize/proposal/non negotiation agreement

--- public/js/search.js
+++ public/js/search.js
@@ -14,3 +14,15 @@ function init() {
   list.cards[2].segs[0].updateUSeg("AHaliq", "haliq@nus.edu.sg");
   // fake data...
 }
+//TODO make create route page
+//TODO from create route, upload to DB route
+//TODO user page 10/11 should generate cards based on DB
+//TODO advertize button should goto advertize details page with get request of route id
+//TODO advertize success will upload advertizement to DB
+//TODO search page should generate cards based on DB
+//TODO user page 20/21 should generate cards based on DB
+//TODO book button should insert proposal to DB
+//TODO cancel button should remove user's proposal from DB
+//TODO or if user is driver, cancel button should remove passengers proposal, and driver's advertizement
+//TODO user page 3 should generate cards based on DB of advertizement that has passed currentDT
+//TOCONSIDER there is no transaction, there is no ride complete, there is no currency alteration
